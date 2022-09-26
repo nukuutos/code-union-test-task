670afef093ce408086fa87bc3c46705776f85a63
nukuutos <nukuutos@gmail.com>
2022-09-27 04:32:06 +1000

    Formate `index.jsx` with prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./components/app";
+import App from './components/app';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import './sass/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
