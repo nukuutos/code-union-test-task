297cce6ce8a4a0c8e420419337493aefabf3a516
nukuutos <nukuutos@gmail.com>
2022-09-29 03:54:11 +1000

    Add `react-query` and `axios` to project

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './components/app';
 import AuthContextProvider from './contexts/auth-context/auth-context';
@@ -8,10 +11,15 @@ import './sass/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const queryClient = new QueryClient();
+
 root.render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
